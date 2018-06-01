fddf69dee32582d2f5a644d3ca8674d5c011108b
perqin <perqinxie@gmail.com>
2018-06-01 19:29:08 +0800

    Include Web GUI v0.0.5

---
+++ scripts/build-web-gui.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Download Web GUI zip
+FILENAME=$(curl 'https://api.github.com/repos/perqin/shadow-dashboard-web-gui/releases/latest' -q | python3 -c "import sys, json; print(json.load(sys.stdin)['assets'][0]['name'])")
+DIRNAME="${FILENAME%.*}"
+DOWNLOAD_URL=$(curl 'https://api.github.com/repos/perqin/shadow-dashboard-web-gui/releases/latest' -q | python3 -c "import sys, json; print(json.load(sys.stdin)['assets'][0]['browser_download_url'])")
+curl $DOWNLOAD_URL -LO
+# Unzip
+unzip -O $FILENAME
+rm $FILENAME
+# Move to src/public
+mv ./$DIRNAME/* ./src/public/
+rm -d $DIRNAME
